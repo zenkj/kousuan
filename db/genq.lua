18bfb42ecbc93436226e993a0c4a4bbbe837056b
juzejian <juzejian@gmail.com>
2017-01-17 18:15:38 +0800

    update

--- db/genq.lua
+++ db/genq.lua
@@ -25,11 +25,43 @@ local function hex(n)
     return high..low
 end
 
--- class = 0: 正常的两操作数运算
--- class = 1: 操作数被除以10的两操作数运算
--- calss = 2: 三操作数运算
-local function say(fd, class, ...)
-    insert(data[fd], '0x'..concat(map({fd+class*64,...}, hex), ''))
+local flagmap = {
+    ['x+y']    = 0,
+    ['x-y']    = 1,
+    ['x*y']    = 2,
+    ['x/y']    = 3,
+    ['x+y/10'] = 0 + 64,
+    ['x-y/10'] = 1 + 64,
+    ['x+y+z']  = 0 + 0*4 + 128,
+    ['x+y-z']  = 0 + 1*4 + 128,
+    ['x+y*z']  = 0 + 2*4 + 128,
+    ['x+y/z']  = 0 + 3*4 + 128,
+    ['x-y+z']  = 1 + 0*4 + 128,
+    ['x-y-z']  = 1 + 1*4 + 128,
+    ['x-y*z']  = 1 + 2*4 + 128,
+    ['x-y/z']  = 1 + 3*4 + 128,
+    ['x*y+z']  = 2 + 0*4 + 128,
+    ['x*y-z']  = 2 + 1*4 + 128,
+    ['x*y*z']  = 2 + 2*4 + 128,
+    ['x*y/z']  = 2 + 3*4 + 128,
+    ['x/y+z']  = 3 + 0*4 + 128,
+    ['x/y-z']  = 3 + 1*4 + 128,
+    ['x/y*z']  = 3 + 2*4 + 128,
+    ['x/y/z']  = 3 + 3*4 + 128
+}
+
+-- flag:
+--   b7 b6 b5 b4 b3 b2 b1 b0
+--   b7: 三操作数运算
+--   b6: 操作数被除以10
+--   b1b0: 第一个操作符: 00:+, 01:-, 10:*, 11:/
+--   b3b2: 第二个操作符（可选）
+--   b5b4: 保留
+-- d1: 第一个操作数
+-- d2: 第二个操作数
+-- d3: 第三个操作数，或者二操作数的结果
+local function say(fd, flag, d1, d2, d3)
+    insert(data[fd], '0x'..concat(map({flagmap[flag],d1,d2,d3}, hex), ''))
 end
 
 local function shuffle(arr)
@@ -46,49 +78,49 @@ end
 -- grade 1 semester 1
 for i = 0, 20 do
     for j = 0, 20 do
-        -- 1. addition within 10
-        if i+j<=10 then say(1, 0, i, j, 0) end
+        -- 1. addition within 10 (x+y)
+        if i+j<=10 then say(1, 'x+y', i, j, i+j) end
 
-        -- 2. subtraction within 10
-        if i<=10 and i-j>=0 then say(2, 0, i, j, 0) end
+        -- 2. subtraction within 10 (x-y)
+        if i<=10 and i-j>=0 then say(2, 'x-y', i, j, i-j) end
 
-        -- 3. carry-save addition within 20
-        if (i>=10 or j>=10) and i+j<=20 then say(3, 0, i, j, 0) end
+        -- 3. carry-save addition within 20 (x+y)
+        if (i>=10 or j>=10) and i+j<=20 then say(3, 'x+y', i, j, i+j) end
 
-        -- 4. subtraction without abdication within 20
-        if i>=10 and i-j>=0 and i%10>=j%10 then say(4, 0, i, j, 0) end
+        -- 4. subtraction without abdication within 20 (x-y)
+        if i>=10 and i-j>=0 and i%10>=j%10 then say(4, 'x-y', i, j, i-j) end
 
-        -- 5. addition with carry within 20
-        if i<10 and j<10 and i+j>10 then say(5, 0, i, j, 0) end
+        -- 5. addition with carry within 20 (x+y)
+        if i<10 and j<10 and i+j>10 then say(5, 'x+y', i, j, i+j) end
 
-        -- 6. subtraction with abdication within 20
-        if i-j>0 and i%10<j%10 then say(6, 0, i, j, 0) end
+        -- 6. subtraction with abdication within 20 (x-y)
+        if i-j>0 and i%10<j%10 then say(6, 'x-y', i, j, i-j) end
 
         -- 7-14 3 operands
         for k=0,20 do
-            -- 7. continuous addition within 10
-            if i+j+k <= 10 then say(7, 2, i, j, k) end
+            -- 7. continuous addition within 10 (x+y+z)
+            if i+j+k <= 10 then say(7, 'x+y+z', i, j, k) end
 
-            -- 8. continuous subtraction within 10
-            if i<=10 and i-j-k>=0 then say(8, 2, i, j, k) end
+            -- 8. continuous subtraction within 10 (x-y-z)
+            if i<=10 and i-j-k>=0 then say(8, 'x-y-z', i, j, k) end
 
-            -- 9. subtraction and addition within 10
-            if i<=10 and i-j>=0 and i-j+k<=10 then say(9, 2, i, j, k) end
+            -- 9. subtraction and addition within 10 (x-y+z)
+            if i<=10 and i-j>=0 and i-j+k<=10 then say(9, 'x-y+z', i, j, k) end
 
-            -- 10. addition and subtraction within 10
-            if i+j<=10 and i+j-k>=0 then say(10, 2, i, j, k) end
+            -- 10. addition and subtraction within 10 (x+y-z)
+            if i+j<=10 and i+j-k>=0 then say(10, 'x+y-z', i, j, k) end
 
-            -- 11. continuous addition within 20
-            if i+j+k>10 and i+j+k<=20 then say(11, 2, i, j, k) end
+            -- 11. continuous addition within 20 (x+y+z)
+            if i+j+k>10 and i+j+k<=20 then say(11, 'x+y+z', i, j, k) end
 
-            -- 12. continuous subtraction within 20
-            if i>10 and i-j-k>=0 then say(12, 2, i, j, k) end
+            -- 12. continuous subtraction within 20 (x-y-z)
+            if i>10 and i-j-k>=0 then say(12, 'x-y-z', i, j, k) end
 
-            -- 13. subtraction and addition within 20
-            if i-j>=0 and i-j+k>10 and i-j+k<=20 then say(13, 2, i, j, k) end
+            -- 13. subtraction and addition within 20 (x-y+z)
+            if i-j>=0 and i-j+k>10 and i-j+k<=20 then say(13, 'x-y+z', i, j, k) end
 
-            -- 14. addition and subtraction within 20
-            if i+j<=20 and i+j-k>=0 then say(14, 2, i, j, k) end
+            -- 14. addition and subtraction within 20 (x+y-z)
+            if i+j<=20 and i+j-k>=0 then say(14, 'x+y-z', i, j, k) end
         end
     end
 end
@@ -96,61 +128,61 @@ end
 -- grade 1 semester 2
 for i=20,100 do
     for j=0,100,10 do
-        -- 15. carry-save addition of whole 10 within 100
-        if i+j<=100 then say(15, 0, i, j, 0); say(15, 0, j, i, 0) end
+        -- 15. carry-save addition of whole 10 within 100 (x+y)
+        if i+j<=100 then say(15, 'x+y', i, j, i+j); say(15, 'x+y', j, i, i+j) end
 
-        -- 16. subtraction without abdication of whole 10 within 100
-        if i-j>=0 then say(16, 0, i, j, 0) end
+        -- 16. subtraction without abdication of whole 10 within 100 (x-y)
+        if i-j>=0 then say(16, 'x-y', i, j, i-j) end
     end 
 end
 
 for i=10,100 do
     for j=0,10-1 do
-        -- 17. carry-save addition of single digit within 100
-        if i+j<=100 and i%10+j<10 then say(17,0,i,j,0); say(17,0,j,i,0) end
+        -- 17. carry-save addition of single digit within 100 (x+y)
+        if i+j<=100 and i%10+j<10 then say(17,'x+y',i,j,i+j); say(17,'x+y',j,i,i+j) end
 
-        -- 18. subtraction without abdication of single digit within 100
-        if i-j>=0 and i%10>=j then say(18,0,i,j,0) end
+        -- 18. subtraction without abdication of single digit within 100 (x-y)
+        if i-j>=0 and i%10>=j then say(18,'x-y',i,j,i-j) end
 
-        -- 19. addition with carry of single digit within 100
-        if i+j<=100 and i%10+j>=10 then say(19,0,i,j,0) end
+        -- 19. addition with carry of single digit within 100 (x+y)
+        if i+j<=100 and i%10+j>=10 then say(19,'x+y',i,j,i+j) end
 
-        -- 20. subtraction with abdication of single digit within 100
-        if i-j>=0 and i%10<j then say(20,0,i,j,0) end
+        -- 20. subtraction with abdication of single digit within 100 (x-y)
+        if i-j>=0 and i%10<j then say(20,'x-y',i,j,i-j) end
     end
 end
 
 for i=10,100-1 do
     for j=10,100-1 do
-        -- 21. carry-save addition of double-digit within 100
-        if i+j<=100 and i%10+j%10<10 then say(21,0,i,j,0) end
+        -- 21. carry-save addition of double-digit within 100 (x+y)
+        if i+j<=100 and i%10+j%10<10 then say(21,'x+y',i,j,i+j) end
 
-        -- 22. subtraction without abdication of double-digit within 100
-        if i-j>=0 and i%10>=j%10 then say(22,0,i,j,0) end
+        -- 22. subtraction without abdication of double-digit within 100 (x-y)
+        if i-j>=0 and i%10>=j%10 then say(22,'x-y',i,j,i-j) end
 
-        -- 23. addition with carry of double-digit within 100
-        if i+j<=100 and i%10+j%10>=10 then say(23,0,i,j,0) end
+        -- 23. addition with carry of double-digit within 100 (x+y)
+        if i+j<=100 and i%10+j%10>=10 then say(23,'x+y',i,j,i+j) end
 
-        -- 24. subtraction without abdication of double-digit within 100
-        if i-j>=0 and i%10<j%10 then say(24,0,i,j,0) end
+        -- 24. subtraction without abdication of double-digit within 100 (x-y)
+        if i-j>=0 and i%10<j%10 then say(24,'x-y',i,j,i-j) end
     end 
 end
 
 -- 25 and 26, 3 operands
--- 25. continuous addition within 100
+-- 25. continuous addition within 100 (x+y+z)
 for i=0,100-1 do
     for j=0,100-1 do
         for k=0,100-1 do
-            if i+j+k<=100 and i+j+k>20 then say(25,2,i,j,k) end
+            if i+j+k<=100 and i+j+k>20 then say(25,'x+y+z',i,j,k) end
         end
     end
 end
 
--- 26. continous subtraction within 100
+-- 26. continous subtraction within 100 (x-y-z)
 for i=21,100-1 do
     for j=0,100-1 do
         for k=0,100-1 do
-            if i-j-k>=0 then say(26,2,i,j,k) end
+            if i-j-k>=0 then say(26,'x-y-z',i,j,k) end
         end
     end
 end
@@ -158,84 +190,84 @@ end
 -- grade 2 semester 1
 for i=0,10-1 do
     for j=0,10-1 do
-        -- 27. multiplication within table
-        say(27,0,i,j,0)
+        -- 27. multiplication within table (x*y)
+        say(27,'x*y',i,j,i*j)
 
-        -- 28. division within table
-        if i~=0 then say(28,0,i*j,i,0) end
+        -- 28. division within table (x/y)
+        if i~=0 then say(28,'x/y',i*j,i,j) end
 
         -- from 29 to 47, there are 3 operands
         for k=0,10-1 do
             if i*j<10 or j*k<10 or i*k<10 then
-                -- 29. continuous multiplication within table
-                say(29,2,i,j,k)
+                -- 29. continuous multiplication within table (x*y*z)
+                say(29,'x*y*z',i,j,k)
 
-                -- 30. continuous division within table
-                if i~=0 and j~=0 then say(30,2,i*j*k,i,j) end
+                -- 30. continuous division within table (x/y/z)
+                if i~=0 and j~=0 then say(30,'x/y/z',i*j*k,i,j) end
             end
-            -- 31. division and multiplication within table
-            if i~=0 then say(31,2,i*j,i,k) end
+            -- 31. division and multiplication within table (x/y*z)
+            if i~=0 then say(31,'x/y*z',i*j,i,k) end
 
-            -- 32. multiplication and division within table
-            if k~=0 and i*j%k==0 then say(32,2,i,j,k) end
+            -- 32. multiplication and division within table (x*y/z)
+            if k~=0 and i*j%k==0 then say(32,'x*y/z',i,j,k) end
         end
 
         for k=0,100-1 do
             if i*j+k<=100 and i*j%10+k%10<10 then
-                -- 33. multiplication and carry-save addition
-                say(33,2,i,j,k)
+                -- 33. multiplication and carry-save addition (x*y+z)
+                say(33,'x*y+z',i,j,k)
 
-                -- 34. carry-save addition and multiplication
-                say(34,2,k,i,j)
+                -- 34. carry-save addition and multiplication (x+y*z)
+                say(34,'x+y*z',k,i,j)
             end
 
-            -- 35. multiplication and subtraction without abdication
-            if i*j-k>=0 and i*j%10>=k%10 then say(35,2,i,j,k) end
+            -- 35. multiplication and subtraction without abdication (x*y-z)
+            if i*j-k>=0 and i*j%10>=k%10 then say(35,'x*y-z',i,j,k) end
 
-            -- 36. subtraction without abdication and multiplication
-            if k-i*j>=0 and k%10>=i*j%10 then say(36,2,k,i,j) end
+            -- 36. subtraction without abdication and multiplication (x-y*z)
+            if k-i*j>=0 and k%10>=i*j%10 then say(36,'x-y*z',k,i,j) end
 
             if i*j+k<=100 and i*j%10+k%10>=10 then
-                -- 37. multiplication and addition with carry
-                say(37,2,i,j,k)
+                -- 37. multiplication and addition with carry (x*y+z)
+                say(37,'x*y+z',i,j,k)
 
-                -- 38. addition with carry and multiplication
-                say(38,2,k,i,j)
+                -- 38. addition with carry and multiplication (x+y*z)
+                say(38,'x+y*z',k,i,j)
             end
 
-            -- 39. multiplication and subtraction with abdication
-            if i*j-k>=0 and i*j%10<k%10 then say(39,2,i,j,k) end
+            -- 39. multiplication and subtraction with abdication (x*y-z)
+            if i*j-k>=0 and i*j%10<k%10 then say(39,'x*y-z',i,j,k) end
 
-            -- 40. subtraction with abdication and multiplication
-            if k-i*j>=0 and k%10<i*j%10 then say(40,2,k,i,j) end
+            -- 40. subtraction with abdication and multiplication (x-y*z)
+            if k-i*j>=0 and k%10<i*j%10 then say(40,'x-y*z',k,i,j) end
 
             if i~=0 and j+k<=100 and j%10+k%10<10 then
-                -- 41. division and carry-save addition
-                say(41,2,i*j,i,k)
+                -- 41. division and carry-save addition (x/y+z)
+                say(41,'x/y+z',i*j,i,k)
 
-                -- 42. carry-save addition and division
-                say(42,2,k,i*j,i)
+                -- 42. carry-save addition and division (x+y/z)
+                say(42,'x+y/z',k,i*j,i)
             end
 
-            -- 43. division and subtraction without abdication
-            if i~=0 and j-k>=0 and j%10>=k%10 then say(43,2,i*j,i,k) end
+            -- 43. division and subtraction without abdication (x/y-z)
+            if i~=0 and j-k>=0 and j%10>=k%10 then say(43,'x/y-z',i*j,i,k) end
 
-            -- 44. subtraction without abdication and division
-            if i~=0 and k-j>=0 and k%10>=j%10 then say(44,2,k,i*j,i) end
+            -- 44. subtraction without abdication and division (x-y/z)
+            if i~=0 and k-j>=0 and k%10>=j%10 then say(44,'x-y/z',k,i*j,i) end
 
             if i~=0 and j+k<=100 and j%10+k%10>=10 then
-                -- 45. division and addition with carry
-                say(45,2,i*j,i,k)
+                -- 45. division and addition with carry (x/y+z)
+                say(45,'x/y+z',i*j,i,k)
 
-                -- 46. addition with carry and division
-                say(46,2,k,i*j,i)
+                -- 46. addition with carry and division (x+y/z)
+                say(46,'x+y/z',k,i*j,i)
             end
 
             -- division and subtraction with abdication (not exist)
-            if i~=0 and j-k>=0 and j%10<k%10 then say('not exist',2,i*j,i,k) end
+            if i~=0 and j-k>=0 and j%10<k%10 then say('not exist','x/y-z',i*j,i,k) end
 
-            -- 47. subtraction with abdication and division
-            if i~=0 and k-j>=0 and k%10<j%10 then say(47,2,k,i*j,i) end
+            -- 47. subtraction with abdication and division (x-y/z)
+            if i~=0 and k-j>=0 and k%10<j%10 then say(47,'x-y/z',k,i*j,i) end
         end
     end
 end
@@ -244,27 +276,27 @@ end
 -- from 48 to 53, all operands are multiples of 10
 for i=100,1000-100,100 do
     for j=100,1000-100,100 do
-        -- 48. addition of whole 100
-        if i+j<=1000 then say(48,1,floor(i/10),floor(j/10),0) end
+        -- 48. addition of whole 100 (x+y)
+        if i+j<=1000 then say(48,'x+y/10',floor(i/10),floor(j/10),floor((i+j)/10)) end
 
-        -- 49. subtraction of whole 100
-        if i>=j then say(49,1,floor(i/10),floor(j/10),0) end
+        -- 49. subtraction of whole 100 (x-y)
+        if i>=j then say(49,'x-y/10',floor(i/10),floor(j/10),floor((i-j)/10)) end
     end
 end
 
 for i=10,1000-10,10 do
     for j=10,1000-10,10 do
-        -- 50. carry-save addition of whole 100 and whole 10
-        if i+j<=1000 and i%100+j%100<100 then say(50,1,floor(i/10),floor(j/10),0) end
+        -- 50. carry-save addition of whole 100 and whole 10 (x+y)
+        if i+j<=1000 and i%100+j%100<100 then say(50,'x+y/10',floor(i/10),floor(j/10),floor((i+j)/10)) end
 
-        -- 51. addition with carry of whole 100 and whole 10
-        if i+j<=1000 and i%100+j%100>=100 then say(51,1,floor(i/10),floor(j/10),0) end
+        -- 51. addition with carry of whole 100 and whole 10 (x+y)
+        if i+j<=1000 and i%100+j%100>=100 then say(51,'x+y/10',floor(i/10),floor(j/10),floor((i+j)/10)) end
 
-        -- 52. subtraction without abdication of whole 100 and whole 10
-        if i>=j and i%100>=j%100 then say(52,1,floor(i/10),floor(j/10),0) end
+        -- 52. subtraction without abdication of whole 100 and whole 10 (x-y)
+        if i>=j and i%100>=j%100 then say(52,'x-y/10',floor(i/10),floor(j/10),floor((i-j)/10)) end
 
-        -- 53. subtraction with abdication of whole 100 and whole 10
-        if i>=j and i%100<j%100 then say(53,1,floor(i/10),floor(j/10),0) end
+        -- 53. subtraction with abdication of whole 100 and whole 10 (x-y)
+        if i>=j and i%100<j%100 then say(53,'x-y/10',floor(i/10),floor(j/10),floor((i-j)/10)) end
     end
 end
 
